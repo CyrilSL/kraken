b29e9ce469213a584798368aa22831749c47e3d2
cyrilsl <lucascyrilsamuel@gmail.com>
2024-05-04 13:30:51 +0530

    disabled admin plugin

--- medusa-config.js
+++ medusa-config.js
@@ -40,19 +40,19 @@ const plugins = [
       upload_dir: "uploads",
     },
   },
-  {
-    resolve: "@medusajs/admin",
-    /** @type {import('@medusajs/admin').PluginOptions} */
-    options: {
-      // only enable `serve` in development
-      // you may need to add the NODE_ENV variable
-      // manually
-      serve: process.env.NODE_ENV === "development",
-      develop: {
-        open: process.env.OPEN_BROWSER !== "false",
-      },
-    },
-  },
+  // {
+  //   resolve: "@medusajs/admin",
+  //   /** @type {import('@medusajs/admin').PluginOptions} */
+  //   options: {
+  //     // only enable `serve` in development
+  //     // you may need to add the NODE_ENV variable
+  //     // manually
+  //     serve: process.env.NODE_ENV === "development",
+  //     develop: {
+  //       open: process.env.OPEN_BROWSER !== "false",
+  //     },
+  //   },
+  // },
   {
     resolve: `medusa-storage-supabase`,
     options: {
