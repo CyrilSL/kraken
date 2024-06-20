c6d36f28e117f306cee0d45ce5ca4185e15e5dca
cyrilsl <lucascyrilsamuel@gmail.com>
2024-06-21 02:12:33 +0530

    enabled redis, deleted one subbscriber..

--- medusa-config.js
+++ medusa-config.js
@@ -117,6 +117,12 @@ const modules = {
       redisUrl: REDIS_URL
     }
   },*/
+  eventBus: {
+    resolve: "@medusajs/event-bus-redis",
+    options: { 
+      redisUrl: process.env.EVENTS_REDIS_URL,
+    },
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
@@ -127,7 +133,7 @@ const projectConfig = {
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
-  // redis_url: REDIS_URL
+  redis_url: REDIS_URL
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
