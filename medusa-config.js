a843de0051d956518c4caadb6280879900762d50
cyrilsl <lucascyrilsamuel@gmail.com>
2024-06-21 03:31:05 +0530

    installed and activated original sendgrid plugin

--- medusa-config.js
+++ medusa-config.js
@@ -81,19 +81,28 @@ const plugins = [
     //  webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
     },
   },
+  // {
+  //   resolve: `medusa-plugin-sendgrid-typescript`,
+  //   /** @type {import('medusa-plugin-sendgrid-typescript').PluginOptions} */
+  //   options: {
+  //     api_key: process.env.SENDGRID_API_KEY,
+  //     from: process.env.SENDGRID_FROM,
+  //     templates: {
+  //       order_placed_template: {
+  //         id: process.env.SENDGRID_ORDER_PLACED_ID,
+  //         // You can add dynamic data to the template by using {variable_name}
+  //         subject: "Thank you for your order !",
+  //       },
+  //     },
+  //   },
+  // },
   {
-    resolve: `medusa-plugin-sendgrid-typescript`,
-    /** @type {import('medusa-plugin-sendgrid-typescript').PluginOptions} */
+    resolve: `medusa-plugin-sendgrid`,
     options: {
       api_key: process.env.SENDGRID_API_KEY,
       from: process.env.SENDGRID_FROM,
-      templates: {
-        order_placed_template: {
-          id: process.env.SENDGRID_ORDER_PLACED_ID,
-          // You can add dynamic data to the template by using {variable_name}
-          subject: "Thank you for your order !",
-        },
-      },
+      order_placed_template: 
+        process.env.SENDGRID_ORDER_PLACED_ID,
     },
   },
   // {
